ba3e2448add89be34f4322ac9f180e82ad5de0c7
adjanorpro18 <64968541+adjanorpro18@users.noreply.github.com>
2022-02-02 15:01:11 +0100

    AF 02/02/202215h00 Home

--- script_app.py
+++ script_app.py
@@ -13,6 +13,11 @@ def home():
     return render_template("home.html", message="Flask_App")
 
 
+@app.route("/machine_learning")
+def machine_learning():
+    return render_template("machine_learning.html", message="Machine Learning")
+
+
 #Exécution de l'application
 
 if __name__ == '__main__':
